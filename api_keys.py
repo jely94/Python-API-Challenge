4b58ac142fa097617927821ac2119f17d49d805f
Jeff Lyng <jely94@icloud.com>
2020-09-14 21:48:13 -0500

    added code and output folder

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "a4dc8840d0d845599237bfb6fed86b11"
+weather_api_key = "weather key"
 
 # Google API Key
-g_key = "AIzaSyAbl5hnvjRKkn1qEN5ioyw0Dz8IU48wRgQ"
+g_key = "google key"
